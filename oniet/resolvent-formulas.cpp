279498231baf8f04f52d5a74e08e0faceec1f737
matuqqq <mateoivanmoreira@gmail.com>
2025-08-27 20:54:17 -0300

    resolvent

---
+++ oniet/resolvent-formulas.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Resolvente(int a, int b, int c) {
+	cout << "raiz 1: " << (-b + (sqrt(((b*b )+ (4 * a * c)) / (2 * a)))) << endl;
+	cout << "raiz 2: "<<  (-b - (sqrt(((b*b) + (4 * a * c)) /( 2 * a))))
+}
+
+int main() {
+	cout << "--- bienvenido a formula cuadratica --- ";
+	cout << endl << "ingrese a (x^2): "
+	int x2=0;
+	cin x2;
+
+	cout << endl << "ingrese b (x): "
+	int x = 0;
+	cin x;
+
+	cout << endl << "ingrese c (c): "
+	int independiente = 0;
+	cin independiente;
+
+}
