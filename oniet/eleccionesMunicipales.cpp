df73fa87f2903016de2010980da400e1f8c1a99d
matuqqq <148290987+matuqqq@users.noreply.github.com>
2025-09-03 16:15:50 -0300

    submit

--- oniet/eleccionesMunicipales.cpp
+++ oniet/eleccionesMunicipales.cpp
@@ -1,99 +1,143 @@
 #include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
 
-using namespace std
-
-void ResFinal(int partPolitico, int cantVotos, cantVotosTotales) {
-	cout << " -------------  Estadisticas del partido " partPolitico << " -------------  " << endl;
-	cout << "cantidad de votos: " cantVotos << endl;
-	cout << "porcentaje sobre el total: " << (cantVotos / cantVotosTotales) * 100 << endl;
+int ExtractS(string texto) {
+	return stoi(texto.substr(0, 1));
 }
-double promedioPunto2() {
-
+int ExtractP(string texto) {
+	return stoi(texto.substr(2,1))
 }
 
+int ExtractR(string texto) {
+	return stoi(texto.substr(6));
+}
 
-double CalcularDesviacion(double promedioPunto2, double valordelatabla) {
-	return (valordelatabla-promedioPunto2)
+void mostrarDetallesFicha() {
+
+	fstream datos("./datos.txt", ios::in)
+	if (!datos) { cout << "hay error pa" << endl; return 0; }
+	string linea = "";
+	while (getline(datos, linea)) {
+		cout << linea << endl;
+	}
+}
+
+void mostrarTablaResultados(vector<vector<int>> fichas) {
+	for (int i=0; i < fichas.size() ;i++) {
+		cout << "seccional: " << i << endl;
+		for (int k = 0;k < fichas[i].size();k++) {
+			cout << "partido: " k << ":  " << fichas[i][k] << endl;
+		}
+	}
+}
+
+vector<int> convFichasATotalVotos(vector<vector<int>> fichas) {
+	vector<int> temp(6,0);
+	for (int i = 0; i < fichas.size();i++) {
+		for (int k = 0; k < fichas[i].size();k++) {
+			temp[k] += fichas[i][k];
+		}
+	}
+	return temp;
+}
+
+void TablaResultados(vector<int> totalVotos) {
+	for (int i = 0; i < totalVotos.size();i++) {
+		cout << "total de votos del partido politico n°: " << i + 1 << " " << totalVotos[i] << endl;
+	}
+}
+
+vector<int> FichasOrdenados(vector<int> fichas) {
+	sort(fichas.rbegin(), fichas.rend());
+	return fichas;
+}
+
+int ObtenerTotalVotos(vector<vector<int>> fichas) {
+	int acumulador=0;
+	vector<int> totalvotos = convFichasATotalVotos(fichas);
+		for (int i = 0;i < totalvotos.size();i++) {
+			acumulador += totalvotos[i];
+		}
+
+	return acumulador;
+}
+
+int convFichasATotalVotosPorSeccional(vector<vector<int>> fichas, int numSeccional) {
+	int acumulador=0;
+	for (int i = 0; i < fichas.size();i++) {
+		for (int k = 0; k < fichas[i].size();k++) {
+			if(numSeccional == i) acumulador++
+		}
+	}
+	return acumulador;
+}
+
+void porcentajeSobreElTotal(int numPartido,vector<vector<int>> fichas){
+	cout << "numero de partido politico: " << numPartido << endl;
+
+	vector<int> fichasPartido = convFichasATotalVotos(fichas)
+	cout << "cantidad de votos de este partido: " << fichasPartido[numPartido] << endl;
+
+	int Total = ObtenerTotalVotos(fichas);
+	cout << "porcentaje obtenido de los votos generales: " << (fichasPartido[numPartido] / Total)*100 << endl;
+}
+
+void ResultadosFinales(vector<vector<int>> fichas) {
+	TablaResultados(convFichasATotalVotos(fichas));
+	TablaResultados(FichasOrdenados(convFichasATotalVotos(fichas));
+	for (int i = 0;i < fichas.size();i++) {
+		for (int k = 0;k < fichas[i].size();k++) {
+			porcentajeSobreEltotal(k, fichas);
+		}
+	}
+}
+
+int seccionalMayVotantes(vector<vector<int>> fichas) {
+	vector<int> TotalVotosPorSeccional;
+	int temp=0;
+	for (int i = 0;i < fichas.size();i++) {
+		TotalVotosPorSeccional[i] = convFichasATotalVotosPorSeccional(i,fichas)
+	}
+	temp=0
+	for (int i = 1;i < TotalVotosPorSeccional.size();i++) {
+	 if(TotalVotosPorSeccional[i]>TotalVotosPorSeccional[i-1]) temp = i
+	}
+	cout << "la seccional con mas habitantes es " << temp << "con " << TotalVotosPorSeccional[i] << " votos" endl;
+
+	}
+
+int promTablaResultados(vector<int> totalVotos) {
+
+	int sumatoria =0
+		for (int i = 0; i < totalVotos.size();i++) {
+			cout << "total de votos del partido politico n°: " << i + 1 << " " << totalVotos[i] << endl;
+			sumatoria += totalVotos[i];
+		}
+	return (sumatoria / totalVotos.size());
+}
+
+void Estadistica(vector<vector<int>> fichas) {
+	seccionalMayVotantes(fichas); //+10p
+	promTablaResultados(convFichasATotalVotos(fichas)); // +10p
+
+	
 }
 
 int main() {
-	int Seccional = 8; //(cant)
-	int partidoPolitico = 6;
-	int NumeroMesa = 10;
-	int totalCantVotos = 0;
-	int resultado[Seccional][partidoPolitico][NumeroMesa]
-	int cantVotos[Seccional][partidoPolitico][NumeroMesa]
-		for (int i = 0;i < Seccional;i++) {
-			for (int j = 0;j < partidoPolitico;j++) {
-				for (int k = 0;k < NumeroMesa;k++) {
-					cout << "ingrese el resultado del numero de mesa" << endl << k << "del partido politico " << endl << j << "de la seccional " << endl << i
-					cin >> resultado[i][j][k];
-					
-					cout << "ingrese la cantidad de votos"<<endl;
-					cin >> cantVotos[i][j][k];
-					totalCantVotos += cantVotos[i][j][k];
-				}
-			}
+	vector<vector<int>> fichas;
+	fstream datos("./datos.txt", ios::in)
+		if (!datos) { cout << "hay error pa" << endl; return 0; }
+		string linea = "";
+		while (getline(datos, linea)) {	
+			fichas[ExtractS(linea)][ExtractP(linea)] += ExtractR(linea);
 		}
+		mostrarDetallesFicha(); // +5p
+		mostrarTablaResultados(fichas); // +20p
+		TablaResultados(convFichasATotalVotos(fichas)); // +10p
+		ResultadosFinales(fichas); // +15p
+		Estadistica();
 
-	int totalVotosSeccPart;
-	int VotosSeccPart[Seccional][partidoPolitico];
-	for (int i = 0;i < Seccional;i++) {
-		for (int j = 0;j < partidoPolitico;j++) {
-			for (int k = 0;k < NumeroMesa;k++) {
-				VotosSeccPart[i][j] += resultado[i][j][k];
-				totalPuntuacionVotosSeccPart += resultado[i][j][k];
-			}
-			cout << "total votos Seccional n°:" << i << "Del partido Politico n° " << j << ":" << endl; << endl;
-			cout << VotosSeccPart[i][j]
-		}
-	}
 
-	int VotosTotalPartido[partidoPolitico];
-	for (int i = 0;i < Seccional;i++) {
-		for (int j = 0;j < partidoPolitico;j++) {
-			for (int k = 0;k < NumeroMesa;k++) {
-				VotosTotalPartido[j] += cantVotos[i][j][k];
-			}
-		
-		}
-	}
 
-	for (int i = 0;i < votosTotalPartido.size();i++) {
-		cout << "votos del partido " << i << ": " << votosTotalPartido[i] << endl;
-	}
-	for (int i = 0; i < partidoPolitico; i++) {
-		ResFinal(i, VotosTotalPartido[i], totalCantVotos);
-	}
-
-	int VotosCantxSeccional[Seccional];
-	for (int i = 0;i < Seccional;i++) {
-		for (int j = 0;j < partidoPolitico;j++) {
-			for (int k = 0;k < NumeroMesa;k++) {
-				VotosCantxSeccional[i] = cantVotos[i][j][k];
-			}
-		}
-	}
-
-	int seccionalMasVotantes = 0;
-	if(seccionalMasVotantes <= VotosCantxSeccional[1]) seccionalMasVotantes=1 
-	for (int i = 1; i < Seccional, i++) {
-		if (VotosCantxSeccional[i] > VotosCantxSeccional[i - 1]) seccionalMasVotantes = i;
-	}
-	
-	cout << "la seccional con mas cantidad de votantes es " << seccionalMasVotantes << endl;
-	cout << "Promedio de resultados punto 2: " totalPuntuacionVotosSeccPart /(Seccional*partidoPolitico);
-	double desviacionesCuadradas[seccional * partidoPolitico];
-	int cont=0;
-	double sumaDesviacionesCuadradas=0;
-	for (int i=0; i < Seccional; i++) {
-		for (int k = 0; k < partidoPolitico;k++)
-
-			desviacionesCuadradas[cont] = (CalcularDesviacion((totalPuntuacionVotosSeccPart / (Seccional * partidoPolitico)), VotosSeccPart[i][partidoPolitico]) * (CalcularDesviacion((totalPuntuacionVotosSeccPart / (Seccional * partidoPolitico)), VotosSeccPart[i][partidoPolitico])))
-			sumaDesviacionesCuadradas += desvacionesCuadradas[cont];
-	}
-	double pasoTres = (sumaDesviacionesCuadradas / cont)
-		double raizCuadrada = sqrt(pasoTres);
-
-	cout << "Desviacion Estandar: "<< raizCuadrada <<endl;
 }
--- oniet/resolvent-formulas.cpp
+++ oniet/resolvent-formulas.cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
-int Resolvente(int a, int b, int c) {
-	cout << "raiz 1: " << (-b + (sqrt(((b*b )+ (4 * a * c)) / (2 * a)))) << endl;
-	cout << "raiz 2: "<<  (-b - (sqrt(((b*b) + (4 * a * c)) /( 2 * a))))
+void Resolvente(int a, int b, int c) {
+	cout << "raiz 1: " << (-b + (sqrt(((b * b) - (4 * a * c))))) / (2 * a) << endl;
+	cout << "raiz 2: " << (-b - (sqrt(((b * b) - (4 * a * c))))) / (2 * a);
 }
 
 int main() {
 	cout << "--- bienvenido a formula cuadratica --- ";
-	cout << endl << "ingrese a (x^2): "
-	int x2=0;
-	cin x2;
+	cout << endl << "ingrese a (x^2): ";
+	int x2 = 0;
+	cin >> x2;
 
-	cout << endl << "ingrese b (x): "
+	cout << endl << "ingrese b (x): ";
 	int x = 0;
-	cin x;
+	cin >> x;
 
-	cout << endl << "ingrese c (c): "
+	cout << endl << "ingrese c (c): ";
 	int independiente = 0;
-	cin independiente;
+	cin >> independiente;
 
+	Resolvente(x2, x, independiente);
 }
